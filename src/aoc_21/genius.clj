f928aa89611b5ffd1b85acb49ac2aaff99a6dd78
Seulvagem <ffelipe.dias@gmail.com>
2021-12-10 12:40:57 -0300

    removed unused file

--- src/aoc_21/genius.clj
+++
@@ -1,9 +0,0 @@
-
-(ns oi
-  (:require [clojure.set :as set]))
-(defn powerset [items] ;; by reborg
-  (reduce
-    (fn [s x]
-      (set/union s (map #(conj % x) s)))
-    (hash-set #{})
-    items))
